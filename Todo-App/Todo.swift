fd8dd37abac91e6afb2bede7c88ad9988001bcf1
nariatsu1202 <atsuki02jojo@gmail.com>
2018-03-23 18:39:20 +0900

    create new file

---
+++ Todo-App/Todo.swift
@@ -0,0 +1,13 @@
+//
+//  Todo.swift
+//  Todo-App
+//
+//  Created by 成田篤基 on 2018/03/23.
+//  Copyright © 2018年 Atsuki Narita. All rights reserved.
+//
+
+import UIKit
+
+class Todo: NSObject {
+
+}
